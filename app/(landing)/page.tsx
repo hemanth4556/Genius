ebc67f3e4587c5df2a5260909cb1fe5c4f92f7e0
Hemanth_kumar <85095216+hemanth4556@users.noreply.github.com>
2023-12-27 15:53:43 +0530

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,24 @@
 import { Button } from "@/components/ui/button";
-import  Link  from "next/link";
-
+import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page (Unprotected)
-      <div>
-        <Link href={"/sign-in"}>
-        <Button>Login</Button></Link>
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Our Awesome App</h1>
+        <p className="text-lg mb-8">Discover a world of possibilities with our amazing features.</p>
 
-      <div>
-        <Link href={"/sign-up"}>
-        <Button>SignUP</Button></Link>
+        <div className="flex space-x-4">
+          <Link href={"/sign-in"}>
+            <Button>Sign In</Button>
+          </Link>
+          <Link href={"/sign-up"}>
+            <Button>Sign Up</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default LandingPage;
